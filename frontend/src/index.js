2dd3a44cddeea0e2e5a623f702d096b3de17bda2
Willie van Staden <60608218+willivstn@users.noreply.github.com>
2023-02-24 12:08:00 +0200

    Added paypal payment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,11 +7,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { StoreProvider } from './Store';
 
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+
 ReactDOM.render(
   <React.StrictMode>
     <StoreProvider>
       <HelmetProvider>
-        <App />
+        <PayPalScriptProvider deferLoading={true}>
+          <App />
+        </PayPalScriptProvider>
       </HelmetProvider>
     </StoreProvider>
   </React.StrictMode>,
