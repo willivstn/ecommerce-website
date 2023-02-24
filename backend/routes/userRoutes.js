c80d60197545f2fb725d51e65b35dd2d6a4fc3fa
Willie van Staden <60608218+willivstn@users.noreply.github.com>
2023-02-24 11:13:01 +0200

    Adder OrderModel and routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bcrypt from 'bcryptjs';
 import expressAsyncHandler from 'express-async-handler';
 import User from '../models/userModel.js';
-import { generateToken } from '../utlis.js';
+import { generateToken } from '../utils.js';
 
 const userRouter = express.Router();
 
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
 import userRouter from './routes/userRoutes.js';
+import orderRouter from './routes/orderRoutes.js';
 
 dotenv.config();
 
@@ -24,8 +25,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
-
 app.use('/api/users', userRouter);
+app.use('/api/orders', orderRouter);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
@@ -33,5 +34,5 @@ app.use((err, req, res, next) => {
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`serve at http://localhost:${port}`);
+  console.log(`serve at http://localhost:${port}/`);
 });
